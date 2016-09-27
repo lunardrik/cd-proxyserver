c9e4800115b852fd8c9dc57bd7b11912a80d676a
Le Minh Quan <lunardrik@gmail.com>
2016-09-27 21:48:13 +0700

    add README.md content

--- README.md
+++ README.md
@@ -1,1 +1,119 @@
-# cd-proxyserver
+# CoderSchool Proxy Server
+
+This is a Proxy Server for Node.js submitted as the [pre-work](http://courses.codepath.com/snippets/intro_to_nodejs/prework) requirement for CodePath.
+
+Time spent: 6.0
+
+Completed:
+
+* [x] Required: Requests to port `8080` are echoed back with the same HTTP headers and body
+* [x] Required: Requests/reponses are proxied to/from the destination server
+* [x] Required: The destination server is configurable via the `--host`, `--port`  or `--url` arguments
+* [x] Required: The destination server is configurable via the `x-destination-url` header
+* [x] Required: Client requests and respones are printed to stdout
+* [x] Required: The `--logfile` argument outputs all logs to the file specified instead of stdout
+* [] Optional: The `--exec` argument proxies stdin/stdout to/from the destination program
+* [x] Optional: The `--loglevel` argument sets the logging chattiness
+* [x] Optional: Supports HTTPS
+* [x] Optional: `-h` argument prints CLI API
+
+Walkthrough Gif:
+
+![Video Walkthrough](walkthrough.gif)
+
+## Starting the Server
+
+```bash
+npm start
+```
+
+## Features
+
+### Echo Server:
+
+```bash
+curl -v -X POST http://127.0.0.1:8080 -d "hello self" -H "x-asdf: yodawg"
+* Rebuilt URL to: http://127.0.0.1:8080/
+*   Trying 127.0.0.1...
+* Connected to 127.0.0.1 (127.0.0.1) port 8080 (#0)
+> POST / HTTP/1.1
+> Host: 127.0.0.1:8080
+> User-Agent: curl/7.43.0
+> Accept: */*
+> x-asdf: yodawg
+> Content-Length: 10
+> Content-Type: application/x-www-form-urlencoded
+>
+* upload completely sent off: 10 out of 10 bytes
+< HTTP/1.1 200 OK
+< host: 127.0.0.1:8080
+< user-agent: curl/7.43.0
+< accept: */*
+< x-asdf: yodawg
+< content-length: 10
+< content-type: application/x-www-form-urlencoded
+< Date: Tue, 27 Sep 2016 12:59:11 GMT
+< Connection: keep-alive
+<
+* Connection #0 to host 127.0.0.1 left intact
+hello self
+```
+
+### Proxy Server:
+
+Port 8081 will proxy to the echo server on port 8000.
+
+```bash
+curl -v http://127.0.0.1:8081/asdf -d "hello proxy"
+*   Trying 127.0.0.1...
+* Connected to 127.0.0.1 (127.0.0.1) port 8081 (#0)
+> POST /asdf HTTP/1.1
+> Host: 127.0.0.1:8081
+> User-Agent: curl/7.43.0
+> Accept: */*
+> Content-Length: 11
+> Content-Type: application/x-www-form-urlencoded
+>
+* upload completely sent off: 11 out of 11 bytes
+< HTTP/1.1 200 OK
+< host: 127.0.0.1:8081
+< user-agent: curl/7.43.0
+< accept: */*
+< content-length: 11
+< content-type: application/x-www-form-urlencoded
+< connection: close
+< date: Tue, 27 Sep 2016 13:24:29 GMT
+<
+* Closing connection 0
+hello proxy
+```
+
+### Configuration:
+
+#### CLI Arguments:
+
+The following CLI arguments are supported:
+
+##### `--host`
+
+The host of the destination server. Defaults to `127.0.0.1`.
+
+##### `--port`
+
+The port of the destination server. Defaults to `80` or `8080` when a host is not specified.
+
+##### `--url`
+
+A single url that overrides the above. E.g., `https://www.amazon.com`
+
+##### `--logfile`
+
+Specify a file path to redirect logging to.
+
+#### Headers
+
+The follow http header(s) are supported:
+
+##### `x-destination-url`
+
+Specify the destination url on a per request basis. Overrides and follows the same format as the `--url` argument.
--- index.js
+++ index.js
@@ -1,11 +1,134 @@
+let https = require('https');
 let http = require('http');
+let path = require('path');
+let fs = require('fs');
+let request = require('request');
+let options = require('./options.json');
+let argv = require('yargs')
+    .usage('Usage: node $0 [options]')
+    .example('node $0 -p 80 -x google.com')
+    .alias('p', 'port')
+    .nargs('p', 1)
+    .describe('p', 'Specify a forwarding port')
+    .alias('x', 'host')
+    .nargs('x', 1)
+    .describe('x', 'Specify a forwarding host')
+    .alias('s', 'port-ssl')
+    .nargs('s', 1)
+    .describe('s', 'Specify a SSL forwarding port')
+    .alias('u', 'host-ssl')
+    .nargs('u', 1)
+    .describe('u', 'Specify a SSL forwarding host')
+    .default('x', '127.0.0.1')
+    .default('u', '127.0.0.1')
+//    .alias('e', 'exec')
+//    .nargs('e', 1)
+//    .describe('e', 'Specify a process to proxy instead')
+    .alias('l', 'log')
+    .nargs('l', 1)
+    .describe('l', 'Specify a output log file')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('Copyright (c) 2016')
+    .argv;
+let ssl_options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+let scheme = 'http://';
+let ssl_scheme = 'https://';
+let port = argv.port || (argv.host === '127.0.0.1' ? 8080 : 80);
+let ssl_port = argv["port-ssl"] || (argv["host-ssl"] === '127.0.0.1' ? 8443 : 443);
+
+let logPath = argv.log && path.join(__dirname, argv.log);
+let logStream = logPath ? fs.createWriteStream(logPath) : process.stdout;
+
+let destinationUrl = argv.url || scheme  + argv.host + ':' + port;
+let destinationSSLUrl = argv.url || ssl_scheme  + argv["host-ssl"] + ':' + ssl_port;
+let proxy_port = 8081;
+let proxy_ssl_port = 8444;
+
+let logLevels = {
+    "OFF": 0,
+    "FATAL": 1,
+    "ERROR": 2,
+    "WARN": 3,
+    "INFO": 4,
+    "DEBUG": 5,
+    "ALL": 6
+};
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+let log = (level, msg) => {
+    if (logLevels[level] <= logLevels[options.logLevel]) {
+        if (typeof msg === 'string') {
+            logStream.write(msg);
+        } else if (typeof msg.pipe === 'function') {
+            msg.pipe(logStream, {end: false});
+        }
+    }
+}
 
 http.createServer((req, res) => {
-  console.log(`New incomming request at: ${req.url}`);
+    log("INFO", `[SRVER] New incoming request at: ${req.url}\n`);
 
-  for(let header in req.headers) {
-    res.setHeader(header, req.headers[header]);
-  }
+    for(let header in req.headers) {
+      res.setHeader(header, req.headers[header]);
+    }
 
-  req.pipe(res);
-}).listen(8080);
+    log("DEBUG", JSON.stringify(req.headers)+'\n');
+    log("DEBUG", req);
+    req.pipe(res);
+}).listen(port, function() {
+    log("INFO", `Server listening on port ${port}\n`);
+});
+
+https.createServer(ssl_options, (req, res) => {
+    log("INFO", `[SRVER] New incoming SSL request at: ${req.url}\n`);
+
+    for(let header in req.headers) {
+      res.setHeader(header, req.headers[header]);
+    }
+
+    log("DEBUG", JSON.stringify(req.headers)+'\n');
+    log("DEBUG", req);
+    req.pipe(res);
+}).listen(ssl_port, function() {
+    log("INFO", `SSL Server listening on port ${ssl_port}\n`);
+});
+
+http.createServer((req, res) => {
+    let outgoing_destination = req.headers['x-destination-url'] || destinationUrl;
+    log("INFO", `[PROXY] Proxying incoming request to: ${outgoing_destination}${req.url}\n`);
+    let options = {
+        method: req.method,
+        headers: req.headers,
+        url: `${outgoing_destination}${req.url}`
+    };
+
+    let downstreamResponse = req.pipe(request(options));
+    log("DEBUG", JSON.stringify(downstreamResponse.headers)+'\n');
+    log("DEBUG", downstreamResponse);
+    downstreamResponse.pipe(res);
+}).listen(proxy_port, function() {
+    log("INFO", `Proxy listening on port ${proxy_port}\n`);
+});
+
+https.createServer(ssl_options, (req, res) => {
+    let outgoing_destination = req.headers['x-destination-url'] || destinationSSLUrl;
+    log("INFO", `[PROXY] Proxying incoming SSL request to: ${outgoing_destination}${req.url}\n`);
+    let options = {
+        method: req.method,
+        headers: req.headers,
+        url: `${outgoing_destination}${req.url}`
+    };
+
+    let downstreamResponse = req.pipe(request(options));
+    log("DEBUG", JSON.stringify(downstreamResponse.headers)+'\n');
+    log("DEBUG", downstreamResponse);
+    downstreamResponse.pipe(res);
+}).listen(proxy_ssl_port, function() {
+    log("INFO", `SSL Proxy listening on port ${proxy_ssl_port}\n`);
+});
